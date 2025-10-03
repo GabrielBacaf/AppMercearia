8486c64e45854b1c2c0ffbf49f6ebc55f6dd1f91
GabrielBacaf <gabryelbaca1@gmail.com>
2025-10-03 16:15:20 -0400

    fix: rename DELETE and LIST cases to DESTROY and INDEX in UserPermissionEnum

--- app/Enums/UserPermissionEnum.php
+++ app/Enums/UserPermissionEnum.php
@@ -7,9 +7,9 @@ enum UserPermissionEnum:string
     case CREATE = 'create users';
     case READ = 'read users';
     case UPDATE = 'update users';
-    case DELETE = 'delete users';
+    case DESTROY = 'destroy users';
     case SHOW = 'show users';
-    case LIST = 'list users';
+    case INDEX = 'index users';
 
     public static function values(): array
     {
