2e1ae1632d80a52ae7695051b23e0dea6099a31a
GabrielBacaf <gabryelbaca1@gmail.com>
2025-10-03 16:16:33 -0400

    refactor: melhorar a estrutura do UserController e adicionar autorizações para ações de usuário

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,38 +2,44 @@
 
 namespace App\Http\Controllers;
 
-
+use App\Enums\UserPermissionEnum;
+use App\Http\Requests\User\StoreUserRequest;
+use App\Http\Requests\User\UpdateUserRequest;
 use App\Http\Services\UserService;
-use App\Http\Controllers\Controller;
-use App\Http\Requests\StoreUserRequest;
-use App\Http\Requests\UpdateUserRequest;
 use App\Models\User;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Hash;
+
 
 class UserController extends Controller
 {
 
     public function __construct(protected UserService $userService)
     {
-        $this->authorizeResource(User::class, 'user');
+
     }
 
     public function index(): JsonResponse
     {
-        $users = User::paginate(15);
+        $this->authorize(UserPermissionEnum::INDEX->value);
+
+        $users = User::paginate(5);
         return response()->json($users);
     }
 
 
     public function store(StoreUserRequest $request): JsonResponse
     {
+
+        $this->authorize(UserPermissionEnum::CREATE->value);
+
         $validatedData = $request->validated();
 
         $user = User::create($validatedData);
 
         return response()->json([
-            'data'    => $user,
+            'data' => $user,
             'message' => 'Usuário criado com sucesso!',
         ], 201);
     }
@@ -43,26 +49,35 @@ class UserController extends Controller
      */
     public function show(User $user): JsonResponse
     {
+        $this->authorize(UserPermissionEnum::SHOW->value);
         return response()->json($user);
     }
 
     public function update(UpdateUserRequest $request, User $user): JsonResponse
     {
+
+
         $validatedData = $request->validated();
 
         if (isset($validatedData['password'])) {
             $validatedData['password'] = Hash::make($validatedData['password']);
         }
-
         $user->update($validatedData);
 
-        return response()->json($user);
+        return response()->json([
+            'data' => $user,
+            'message' => 'Usuário atualizado com sucesso!',
+        ]);
+
     }
 
 
     public function destroy(User $user): JsonResponse
     {
         $user->delete();
-        return response()->json(null, 204);
+
+        return response()->json([
+            'message' => 'Usuário deletado com sucesso!',
+        ], 204);
     }
 }
