321a95ed746e884ca7b2637dfe92341a187b79d7
GabrielBacaf <gabryelbaca1@gmail.com>
2025-10-11 22:16:23 -0400

    refactor: retirando comentarios desnecessarios

--- app/Http/Traits/ApiResponse.php
+++ app/Http/Traits/ApiResponse.php
@@ -5,19 +5,10 @@ namespace App\Http\Traits;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Resources\Json\JsonResource;
-use Symfony\Component\HttpFoundation\Response;
-use Illuminate\Validation\Validator;
 
 trait ApiResponse
 {
-    /**
-     * Retorna uma resposta de sucesso padronizada.
-     *
-     * @param mixed $data
-     * @param string $message
-     * @param int $status
-     * @return \Illuminate\Http\JsonResponse
-     */
+
     protected function successResponse(array|Model|JsonResource|null $data = [], string $message = '', int $status = 200): JsonResponse
     {
         return response()->json([
@@ -27,14 +18,7 @@ trait ApiResponse
         ], $status);
     }
 
-    /**
-     * Retorna uma resposta de erro padronizada.
-     *
-     * @param string $message
-     * @param array $errors
-     * @param int $status
-     * @return \Illuminate\Http\JsonResponse
-     */
+
     protected function errorResponse(string $message, array $errors = [], int $status): JsonResponse
     {
         return response()->json([
@@ -44,15 +28,6 @@ trait ApiResponse
         ], $status);
     }
 
-    /**
-     * Retorna uma resposta de sucesso padronizada.
-     *
-     * @param mixed $data
-     * @param string $message
-     * @param int $status
-     * @param mixed $resource
-     * @return \Illuminate\Http\JsonResponse
-     */
     protected function successResponseCollection(array|Model|JsonResource|null $data = [], mixed $resource, string $message = '', int $status = 200): JsonResponse
     {
         return response()->json([
