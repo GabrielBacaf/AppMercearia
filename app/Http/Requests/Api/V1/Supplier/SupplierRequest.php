23de9fddea3e6e27106977c1c0edf2fed239d23c
GabrielBacaf <gabryelbaca1@gmail.com>
2025-10-17 23:15:17 -0400

    feat:criando o request do fornecedor

---
+++ app/Http/Requests/Api/V1/Supplier/SupplierRequest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Requests\Api\V1\Supplier;
+
+use App\Enums\SupplierPermissionEnum;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Validation\Rule;
+
+class SupplierRequest extends FormRequest
+{
+
+    public function authorize(): bool
+    {
+        return $this->user()?->can(SupplierPermissionEnum::STORE->value);
+    }
+
+    public function rules(): array
+    {
+
+
+        return [
+            'fantasy_name' => ['required', 'string', 'max:60'],
+            'legal_name' => ['required', 'string', 'max:70', Rule::unique('suppliers', 'legal_name')->ignore($this->supplier)],
+            'cnpj' => ['required', 'string', 'max:14' , Rule::unique('suppliers', 'cnpj')->ignore($this->id)->ignore($this->supplier)],
+
+        ];
+    }
+
+    public function attributes(): array
+    {
+        return [
+            'fantasy_name' => 'Nome Fantasia',
+            'legal_name' => 'Razão Social',
+            'cnpj' => 'CNPJ',
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success' => false,
+            'message' => 'Erro de validação',
+            'errors' => $validator->errors()
+        ], 422));
+    }
+}
