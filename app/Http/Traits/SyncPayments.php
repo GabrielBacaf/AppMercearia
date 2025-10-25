c243939de5a83e26a887cda5ed4efcf0984687c1
GabrielBacaf <gabryelbaca1@gmail.com>
2025-10-25 15:20:49 -0400

    feat: implement SyncPayments trait for managing payment synchronization

---
+++ app/Http/Traits/SyncPayments.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Traits;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Arr;
+v
+
+trait SyncPayments
+{
+    public static function syncPayments(Model $model, array $paymentsData): void
+    {
+        $incomingIds = array_filter(Arr::pluck($paymentsData, 'id'));
+
+        if (count($incomingIds) > 0) {
+            $model->payments()->whereNotIn('id', $incomingIds)->delete();
+        } else {
+            $model->payments()->delete();
+        }
+
+        foreach ($paymentsData as $paymentData) {
+            $model->payments()->updateOrCreate(
+                [
+                    'id' => $paymentData['id'] ?? null
+                ],
+                $paymentData
+            );
+        }
+    }
+}
