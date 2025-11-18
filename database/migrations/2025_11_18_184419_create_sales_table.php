a2c226c2aaf2fde77e7e3b19f3de9dc0063609b8
GabrielBacaf <gabryelbaca1@gmail.com>
2025-11-18 19:56:59 -0400

    feat: criando as migration

---
+++ database/migrations/2025_11_18_184419_create_sales_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Enums\PaymentStatusEnum;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('discount', 2);
+            $table->decimal('delivery_price', 2);
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('updated_by')->nullable()->constrained('users');
+            $table->foreignId('client_id')->nullable()->constrained('clients');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sales');
+    }
+};
