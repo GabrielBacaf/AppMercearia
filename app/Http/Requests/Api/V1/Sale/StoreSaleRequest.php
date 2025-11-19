ef8fba143d658a7d70ba0e8775f5f86dc2c34c6d
GabrielBacaf <gabryelbaca1@gmail.com>
2025-11-19 09:21:58 -0400

    refactor: terminando o request das vendas e removendo comentario de compra

--- app/Http/Requests/Api/V1/Sale/StoreSaleRequest.php
+++ app/Http/Requests/Api/V1/Sale/StoreSaleRequest.php
@@ -3,7 +3,10 @@
 namespace App\Http\Requests\Api\V1\Role;
 
 use App\Enums\SalePermissionEnum;
+use Illuminate\Validation\Rule;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
 
 
 class StoreSaleRequest extends FormRequest
@@ -22,18 +25,29 @@ class StoreSaleRequest extends FormRequest
             'updated_by' => ['prohibited'],
             'client_id' => ['sometimes', 'integer', 'exists:clients,id'],
             'products' => ['required', 'array', 'min:1'],
+            'products.*' => ['sometimes', 'string', Rule::exists('id', 'products')]
         ];
-
     }
 
     public function attributes(): array
     {
         return [
+            'discount' => 'DESCONTO',
+            'delivery_price' => 'PREÇO DE ENTREGA',
+            'user_id' => 'USUÁRIO',
+            'updated_by' => 'USUARIO',
+            'client_id' => 'CLIENTE',
+            'products' => 'PRODUTO'
 
         ];
     }
 
-
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success' => false,
+            'message' => 'Erro de validação',
+            'errors' => $validator->errors()
+        ], 422));
+    }
 }
-
-
