0ff8090025f74153f8d22e67c99e3409347e1d65
GabrielBacaf <gabryelbaca1@gmail.com>
2026-03-29 20:24:23 -0400

    feat: atualizar StoreSaleRequest com novas regras de validação e atributos para pagamentos

--- app/Http/Requests/Api/V1/Sale/StoreSaleRequest.php
+++ app/Http/Requests/Api/V1/Sale/StoreSaleRequest.php
@@ -1,7 +1,9 @@
 <?php
 
-namespace App\Http\Requests\Api\V1\Role;
+namespace App\Http\Requests\Api\V1\Sale;
 
+use App\Enums\PaymentStatusEnum;
+use App\Enums\PaymentTypeEnum;
 use App\Enums\SalePermissionEnum;
 use Illuminate\Validation\Rule;
 use Illuminate\Foundation\Http\FormRequest;
@@ -16,16 +18,25 @@ class StoreSaleRequest extends FormRequest
         return $this->user()?->can(SalePermissionEnum::STORE->value);
     }
 
+
     public function rules(): array
     {
         return [
-            'discount' => ['sometimes', 'decimal'],
-            'delivery_price' => ['sometimes', 'decimal'],
+            'discount' => ['sometimes', 'numeric', 'min:0' , 'max:10'],
+            'delivery_price' => ['sometimes', 'numeric', 'min:0'],
             'user_id' => ['prohibited'],
             'updated_by' => ['prohibited'],
+            'total_value' => ['prohibited'],
             'client_id' => ['sometimes', 'integer', 'exists:clients,id'],
+
             'products' => ['required', 'array', 'min:1'],
-            'products.*' => ['sometimes', 'string', Rule::exists('id', 'products')]
+            'products.*.id' => ['sometimes', 'integer', Rule::exists('products', 'id')],
+            'products.*.quantity' => ['required', 'integer', 'min:1'],
+
+            'payments' => ['required', 'array', 'min:1'],
+            'payments.*.payment_type' => ['required', 'string', Rule::in(PaymentTypeEnum::values())],
+            'payments.*.payment_status' => ['required', 'string', Rule::in(PaymentStatusEnum::values())],
+            'payments.*.value' => ['required', 'numeric', 'min:0.01', ],
         ];
     }
 
@@ -37,9 +48,25 @@ class StoreSaleRequest extends FormRequest
             'user_id' => 'USUÁRIO',
             'updated_by' => 'USUARIO',
             'client_id' => 'CLIENTE',
-            'products' => 'PRODUTO'
+            'products' => 'PRODUTO',
+            'products.*.id' => 'ID DO PRODUTO',
+            'products.*.quantity' => 'QUANTIDADE DO PRODUTO',
+            'payments' => 'PAGAMENTOS',
+            'payments.*.payment_type' => 'TIPO DE PAGAMENTO',
+            'payments.*.payment_status' => 'STATUS DO PAGAMENTO',
+            'payments.*.value' => 'VALOR DO PAGAMENTO',
+            'total_value' => 'VALOR TOTAL',
 
         ];
+    }
+
+    public function validated($key = null, $default = null)
+    {
+        $validatedData = parent::validated($key, $default);
+
+        $validatedData['user_id'] = $this->user()->id;
+
+        return $validatedData;
     }
 
     protected function failedValidation(Validator $validator)
